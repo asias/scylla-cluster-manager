cf93cad95ff1bcde821076781ff5dc8f188b2025
Asias He <asias@scylladb.com>
2021-01-16 13:48:54 +0800

    Drop the unsupported version

--- utils.py
+++ utils.py
@@ -10,8 +10,7 @@ scylla_package_list = {
     'Local ./scylla-package.tar.gz': '',
     'Scylla 4.4.dev': f'{SCYLLA_DOWNLOAD}/relocatable/unstable/master/2020-12-20T00%3A11%3A59Z/scylla-unified-package-4.4.dev.0.20201220.b3e39d81aa8.tar.gz',
     'Scylla 4.3': f'{SCYLLA_DOWNLOAD}/downloads/scylla/relocatable/scylladb-4.3/scylla-unified-package-4.3.0.0.20210110.000585522.tar.gz',
-    'Scylla Enterprise 2021.1.dev': f'{SCYLLA_DOWNLOAD}/enterprise/relocatable/unstable/enterprise/2020-12-31T11%3A42%3A35Z/scylla-enterprise-unified-package-2021.1.dev.0.20201231.b47bcd5ff.tar.gz',
-    'Scylla Enterprise 2020.1 (Unsupported)': ''
+    'Scylla Enterprise 2021.1.dev': f'{SCYLLA_DOWNLOAD}/enterprise/relocatable/unstable/enterprise/2020-12-31T11%3A42%3A35Z/scylla-enterprise-unified-package-2021.1.dev.0.20201231.b47bcd5ff.tar.gz'
 }
 
 
